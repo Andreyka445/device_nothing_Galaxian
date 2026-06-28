42e7cf932dca97b92d2834b671d6379a315359a1
Andreyka445 <seleznevandrey186@outlook.com>
2026-06-28 20:56:31 +0000

    Galaxian: move props to properties dir
    Signed-off-by: Andreyka445 <seleznevandrey186@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,10 +136,10 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 BOARD_USES_METADATA_PARTITION := true
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_ODM_PROP += $(DEVICE_PATH)/properties//odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/properties/system_ext.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
